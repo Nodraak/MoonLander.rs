dbd101248cb255c51874180c2fc3459a9fa13a45
Adrien Chardon <adrienchardon@mailoo.org>
2021-06-20 12:08:23 +0200

    WIP - noise and bias

--- src/adapters/sim.rs
+++ src/adapters/sim.rs
@@ -30,11 +30,20 @@ fn init_(conf: Conf) -> Result<AdapterSim, &'static str> {
 
 impl Adapter for AdapterSim {
     fn read_sensors(&mut self) -> SensorsValues {
-        self.sim.read_sensors()
+        let mut ret = self.sim.read_sensors();
+
+        // inject noise and bias
+
+        ret
     }
 
     fn write_actuators(&mut self, control: ActuatorsValues) {
-        self.sim.write_actuators(control)
+        let mut c = control;
+
+        // inject noise and bias
+        // c.engine_throttle *= 0.98;
+
+        self.sim.write_actuators(c)
     }
 
     fn export_to_csv_conf(&self) {
