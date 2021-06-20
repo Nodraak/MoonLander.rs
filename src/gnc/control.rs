Nodraak/MoonLander.rs

commit 52ebe46b6a7ce531af96f40474df19fbd7ae2674

[src/gnc/nav] Mod 360

src/gnc/navigation.rs
@@ -1,5 +1,6 @@
 use std::f64::consts::PI;
 use uom::si::f64::*;
+use uom::si::angle::degree;
 use uom::si::velocity::meter_per_second;
 
 use crate::{mul, norm, sqrt, squared};
@@ -41,6 +42,13 @@ pub fn nav(spacecraft: &mut Spacecraft, sensors_vals: &SensorsValues) {
     spacecraft.cur.ang_vel += dav;
     let dap: Angle = (spacecraft.cur.ang_vel*dt).into();
     spacecraft.cur.ang_pos += dap;
+
+    while spacecraft.cur.ang_pos > Angle::new::<degree>(180.0) {
+        spacecraft.cur.ang_pos -= Angle::new::<degree>(360.0);
+    }
+    while spacecraft.cur.ang_pos < -Angle::new::<degree>(180.0) {
+        spacecraft.cur.ang_pos += Angle::new::<degree>(360.0);
+    }
 
     // TODO cross check altitude - kalman filter?
 

commit 9afb4c028f7c4b99d9c60905be718571e5ad14ef

src/gnc: fix bug - modulo
Bug introduced in 52ebe46b6a7ce531af96f40474df19fbd7ae2674

src/gnc/control.rs
@@ -6,7 +6,7 @@ use uom::si::angle::{degree, radian};
 use uom::si::ratio::ratio;
 use uom::si::time::second;
 
-use crate::{sqrt, squared};
+use crate::{sqrt, squared, modulo};
 use crate::adapters::common::ActuatorsValues;
 use crate::conf::{Scenario, CtrSpacecraft};
 use crate::gnc::common::Spacecraft;
@@ -144,8 +144,7 @@ fn control_angular(
     let control_transfer_function = 1.0 / squared!(Time::new::<second>(1.0));
 
     // ang acc PID
-
-    let err: Angle = ctr_ang_pos - sc_ang_pos;
+    let err: Angle = modulo!(ctr_ang_pos - sc_ang_pos, Angle::new::<degree>(360.0));
     let derr: AngularVelocity = sc_ang_vel;  // TODO try derive err
     let control: Angle = (kp*err + kd*derr).into();
     let ctr_ang_acc: AngularAcceleration = (control * control_transfer_function).into();

src/gnc/navigation.rs
@@ -3,7 +3,7 @@ use uom::si::f64::*;
 use uom::si::angle::degree;
 use uom::si::velocity::meter_per_second;
 
-use crate::{mul, norm, sqrt, squared};
+use crate::{mul, norm, sqrt, squared, modulo};
 use crate::adapters::common::SensorsValues;
 use crate::gnc::common::Spacecraft;
 use crate::utils::math::Vec2;
@@ -42,13 +42,7 @@ pub fn nav(spacecraft: &mut Spacecraft, sensors_vals: &SensorsValues) {
     spacecraft.cur.ang_vel += dav;
     let dap: Angle = (spacecraft.cur.ang_vel*dt).into();
     spacecraft.cur.ang_pos += dap;
-
-    while spacecraft.cur.ang_pos > Angle::new::<degree>(180.0) {
-        spacecraft.cur.ang_pos -= Angle::new::<degree>(360.0);
-    }
-    while spacecraft.cur.ang_pos < -Angle::new::<degree>(180.0) {
-        spacecraft.cur.ang_pos += Angle::new::<degree>(360.0);
-    }
+    spacecraft.cur.ang_pos = modulo!(spacecraft.cur.ang_pos, Angle::new::<degree>(360.0));
 
     // TODO cross check altitude - kalman filter?
 

src/utils/uom.rs
@@ -28,3 +28,18 @@ macro_rules! norm {
         sqrt!(squared!($quantity.x) + squared!($quantity.y))
     }};
 }
+
+
+#[macro_export]
+macro_rules! modulo {
+    ($val:expr, $modval:expr) => {{
+        let mut ret = $val;
+        while ret > ($modval/2.0).into() {
+            ret -= $modval;
+        }
+        while ret < (-$modval/2.0).into() {
+            ret += $modval;
+        }
+        ret
+    }};
+}